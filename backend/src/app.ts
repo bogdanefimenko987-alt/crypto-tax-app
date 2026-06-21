745f3db295411df3531fb707bf7bd44dc78a6892
Bogdan <bogdanefimenko987@email.com>
2026-06-21 19:12:45 +0300

    Fix CORS or other issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,10 @@ import categoriesRoutes from './portfolio/categoriesRoutes';
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'https://crypto-tax-app-7p4g.vercel.app',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
